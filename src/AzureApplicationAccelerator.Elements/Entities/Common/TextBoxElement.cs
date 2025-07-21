9f1ba605f19eb5e61bffec73c8e7c0b25b5beaf9
emileSWAAA <emileverbunt@microsoft.com>
2025-07-21 14:14:49 +0200

    Implement text box component with validation features
    Updated `TextBox.razor` to support single-line and multi-line inputs based on the `MultiLine` property. Removed placeholder text and added a new structure for rendering.
    
    Enhanced `TextBoxEditor.razor` to include configuration options for default value, placeholder, and multi-line input, along with a constraints section for adding validation rules (Regex and IsValid).
    
    Modified `TextBoxElement.cs` to introduce new properties for `DefaultValue`, `Placeholder`, and `MultiLine`. Added `TextBoxConstraints` class to manage validation constraints, along with abstract and derived classes for validation types.

--- src/AzureApplicationAccelerator.Elements/Entities/Common/TextBoxElement.cs
+++ src/AzureApplicationAccelerator.Elements/Entities/Common/TextBoxElement.cs
@@ -1,6 +1,42 @@
+using AzureApplicationAccelerator.Elements.Interfaces;
+using System.Text.Json.Serialization;
+
 namespace AzureApplicationAccelerator.Elements.Entities.Common
 {
     public class TextBoxElement : UIElement
     {
+        [JsonPropertyName("defaultValue")]
+        public string? DefaultValue { get; set; }
+
+        public string? Placeholder { get; set; }
+
+        [JsonPropertyName("multiLine")]
+        public bool? MultiLine { get; set; }
+
+        public TextBoxConstraints Constraints { get; set; } = new TextBoxConstraints();
+    }
+
+    public class TextBoxConstraints : IRequiredConstraint
+    {
+        public bool? Required { get; set; }
+
+        public IList<TextBoxConstraintValidation>? Validations { get; set; } = new List<TextBoxConstraintValidation>();
+    }
+
+    public abstract class TextBoxConstraintValidation
+    {
+        public string? Message { get; set; }
+    }
+
+    public class TextBoxConstraintRegexValidation : TextBoxConstraintValidation
+    {
+        public string? Regex { get; set; }
+
+    }
+
+    public class TextBoxConstraintIsValidValidation : TextBoxConstraintValidation
+    {
+        [JsonPropertyName("isValid")]
+        public string? IsValid { get; set; }
     }
 }
